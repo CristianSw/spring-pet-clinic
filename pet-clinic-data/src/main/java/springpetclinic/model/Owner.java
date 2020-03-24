105f9b211b984413f0c79ee739b1df1519c8deaf
CristianSw <cristian.sw@protonmail.com>
2020-03-24 14:35:19 +0200

    Add BaseEntity class for providing ID to models

--- pet-clinic-data/src/main/java/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/springpetclinic/model/Owner.java
@@ -1,13 +1,5 @@
 package springpetclinic.model;
 
 public class Owner extends Person {
-    private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
