488672d6fbec821e5a1164b5d9e81b2402e44ccb
CristianSw <cristian.sw@protonmail.com>
2020-03-25 22:14:40 +0200

    Owner and Vet Mapping reviewed implementation

--- pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/OwnerController.java
@@ -2,11 +2,11 @@ package com.teza.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
-    @RequestMapping({"/owners","owners/index","owners/index.html"})
-    public String listOfVets(){
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOfOwners(){
         return "owners/index";
     }
 }
