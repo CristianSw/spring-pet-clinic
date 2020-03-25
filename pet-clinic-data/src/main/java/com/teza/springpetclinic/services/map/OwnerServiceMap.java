a303883f915832a22b964b544d6a3640bdfba130
CristianSw <cristian.sw@protonmail.com>
2020-03-25 20:55:42 +0200

    Implement Map Based Services

---
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.teza.springpetclinic.services.map;
+
+import com.teza.springpetclinic.model.Owner;
+import com.teza.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(final Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(final Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(final Long id) {
+        return super.findById(id);
+    }
+}
