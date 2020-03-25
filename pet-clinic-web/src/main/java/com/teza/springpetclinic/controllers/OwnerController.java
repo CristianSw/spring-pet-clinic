8dc3a13ba60e2532ca3207c961d5f0a777c35005
CristianSw <cristian.sw@protonmail.com>
2020-03-25 22:25:50 +0200

    Formating of controllers

--- pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/OwnerController.java
@@ -2,11 +2,12 @@ package com.teza.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
-    @RequestMapping({"","/","/index","/index.html"})
-    public String listOfOwners(){
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOfOwners() {
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/VetController.java
@@ -2,11 +2,12 @@ package com.teza.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequestMapping({"/vets"})
 @Controller
 public class VetController {
-    @RequestMapping({"","/","/index","/index.html"})
-    public String listOfVets(){
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOfVets() {
         return "vets/index";
     }
 }
