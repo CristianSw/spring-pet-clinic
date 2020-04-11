ea5ed14371725b016f6aa2612056ca16937b434e
Cristian <cristian.sw@protonmail.com>
2020-04-11 18:23:16 +0300

    Fix VitServiceMap error, added validation for save method

--- pet-clinic-data/src/main/java/com/teza/springpetclinic/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/services/map/VisitServiceMap.java
@@ -9,11 +9,6 @@ import java.util.Set;
 @Service
 public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
 
-    private final VisitService visitService;
-
-    public VisitServiceMap(VisitService visitService) {
-        this.visitService = visitService;
-    }
 
     @Override
     public Set<Visit> findAll() {
@@ -31,8 +26,13 @@ public class VisitServiceMap extends AbstractMapService<Visit, Long> implements 
     }
 
     @Override
-    public Visit save(Visit object) {
-        return super.save(object);
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId()== null
+        || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
     }
 
     @Override
