5bb1b4219390a501c389742aef3836be9efa0a87
Cristian <cristian.sw@protonmail.com>
2020-04-11 16:38:06 +0300

    Added JPA Repositories

---
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/repositories/VetRepository.java
@@ -0,0 +1,7 @@
+package com.teza.springpetclinic.repositories;
+
+import com.teza.springpetclinic.model.Vet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VetRepository extends CrudRepository<Vet, Long> {
+}
