f67e2a24890f8dfae520b4862ed1367711fc9d36
Cristian <cristian.sw@protonmail.com>
2020-04-11 18:10:26 +0300

    added VisitServiceMap implementation

---
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.teza.springpetclinic.services;
+
+import com.teza.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,41 @@
+package com.teza.springpetclinic.services.map;
+
+import com.teza.springpetclinic.model.Visit;
+import com.teza.springpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final VisitService visitService;
+
+    public VisitServiceMap(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
