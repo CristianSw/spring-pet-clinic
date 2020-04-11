ae234ef65d7c6554563ffdf2a91de1ef56c0314e
Cristian <cristian.sw@protonmail.com>
2020-04-11 18:14:14 +0300

    VisitSDJpaService added

--- pet-clinic-data/src/main/java/com/teza/springpetclinic/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/services/map/VisitServiceMap.java
@@ -5,6 +5,7 @@ import com.teza.springpetclinic.services.VisitService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+
 @Service
 public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
 
---
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package com.teza.springpetclinic.services.springdatajpa;
+
+import com.teza.springpetclinic.model.Visit;
+import com.teza.springpetclinic.repositories.VisitRepository;
+import com.teza.springpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
