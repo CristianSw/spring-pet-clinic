3537496be741b92d1ac324b49243b802452d337f
Cristian <cristian.sw@protonmail.com>
2020-04-11 16:14:28 +0300

    Add Vet and Specialties to JPA Model

--- pet-clinic-data/src/main/java/com/teza/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/model/Vet.java
@@ -1,12 +1,14 @@
 package com.teza.springpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 @Entity
 public class Vet extends Person {
-    @OneToMany
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vets",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
