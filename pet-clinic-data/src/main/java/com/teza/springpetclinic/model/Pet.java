36de6295ffd350ffd333293f9f3992d3dddeca71
Cristian <cristian.sw@protonmail.com>
2020-04-11 16:24:28 +0300

    Added Visit entity to JPA Model

--- pet-clinic-data/src/main/java/com/teza/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/model/Pet.java
@@ -2,9 +2,12 @@ package com.teza.springpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
-public class Pet extends BaseEntity{
+public class Pet extends BaseEntity {
     @Column(name = "name")
     private String name;
     @ManyToOne
@@ -15,6 +18,8 @@ public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
 
     public String getName() {
@@ -47,5 +52,13 @@ public class Pet extends BaseEntity{
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
