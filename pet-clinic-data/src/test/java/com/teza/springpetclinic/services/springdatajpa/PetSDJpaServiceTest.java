4f3f261f8c393f100d12ec33db51e1177f8139e8
Cristian <cristian.sw@protonmail.com>
2020-04-15 15:33:10 +0300

    JUnit test provided for OwnerSDJpa service

---
+++ pet-clinic-data/src/test/java/com/teza/springpetclinic/services/springdatajpa/PetSDJpaServiceTest.java
@@ -0,0 +1,36 @@
+package com.teza.springpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PetSDJpaServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
