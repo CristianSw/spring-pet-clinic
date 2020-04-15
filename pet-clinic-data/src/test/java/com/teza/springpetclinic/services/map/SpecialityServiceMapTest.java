2c2096f5917b3fa1957e514e4e89acd481191bfc
Cristian <cristian.sw@protonmail.com>
2020-04-15 10:22:08 +0300

    Provided Junit tests for Speciality Map Service

--- pet-clinic-data/src/test/java/com/teza/springpetclinic/services/map/SpecialityServiceMapTest.java
+++ pet-clinic-data/src/test/java/com/teza/springpetclinic/services/map/SpecialityServiceMapTest.java
@@ -1,33 +1,51 @@
 package com.teza.springpetclinic.services.map;
 
+import com.teza.springpetclinic.model.Speciality;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpecialityServiceMapTest {
+    SpecialityServiceMap specialityServiceMap;
+    Long specId = 1L;
 
     @BeforeEach
     void setUp() {
+        specialityServiceMap = new SpecialityServiceMap();
+        specialityServiceMap.save(Speciality.builder().id(specId).build());
     }
 
     @Test
     void findAll() {
+        Set<Speciality> specialities = specialityServiceMap.findAll();
+        assertEquals(1, specialities.size());
     }
 
     @Test
     void deleteById() {
+        specialityServiceMap.deleteById(specialityServiceMap.findById(specId).getId());
+        assertEquals(0, specialityServiceMap.findAll().size());
     }
 
     @Test
     void delete() {
+        specialityServiceMap.delete(specialityServiceMap.findById(specId));
+        assertEquals(0, specialityServiceMap.findAll().size());
     }
 
     @Test
     void save() {
+        Long id = 2L;
+        specialityServiceMap.save(Speciality.builder().id(id).build());
+        assertEquals(2,specialityServiceMap.findAll().size());
     }
 
     @Test
     void findById() {
+        Speciality specialities =  specialityServiceMap.findById(specId);
+        assertEquals(specialities.getId(),specialityServiceMap.findById(specId).getId());
     }
 }
