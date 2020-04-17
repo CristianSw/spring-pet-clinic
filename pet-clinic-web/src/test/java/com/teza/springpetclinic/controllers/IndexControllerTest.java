d9f53aed5be2a5176c6d297f9a569bc7ec206952
Cristian <cristian.sw@protonmail.com>
2020-04-17 11:03:10 +0300

    Vet Controller MVN Tests

--- pet-clinic-web/src/test/java/com/teza/springpetclinic/controllers/IndexControllerTest.java
+++ pet-clinic-web/src/test/java/com/teza/springpetclinic/controllers/IndexControllerTest.java
@@ -2,17 +2,18 @@ package com.teza.springpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@ExtendWith(MockitoExtension.class)
 class IndexControllerTest {
 
     IndexController controller;
---
+++ pet-clinic-web/src/test/java/com/teza/springpetclinic/controllers/VetControllerTest.java
@@ -0,0 +1,72 @@
+package com.teza.springpetclinic.controllers;
+
+import com.teza.springpetclinic.model.Vet;
+import com.teza.springpetclinic.services.VetService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class VetControllerTest {
+
+    @Mock
+    VetService vetService;
+    @InjectMocks
+    VetController controller;
+    Set<Vet> vet;
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        vet = new HashSet<>();
+        vet.add(Vet.builder().id(1L).build());
+        vet.add(Vet.builder().id(1L).build());
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+    @Test
+    void listOfVets() throws Exception {
+        when(vetService.findAll()).thenReturn(vet);
+        mockMvc.perform(get("/vets.html"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("vets/index"))
+                .andExpect(model().attribute("vets",hasSize(2)));
+    }
+
+    @Test
+    void listOfVetsSlash() throws Exception {
+        when(vetService.findAll()).thenReturn(vet);
+        mockMvc.perform(get("/vets.html/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("vets/index"))
+                .andExpect(model().attribute("vets",hasSize(2)));
+    }
+    @Test
+    void listOfVetsIndex() throws Exception {
+        when(vetService.findAll()).thenReturn(vet);
+        mockMvc.perform(get("/vets.html/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("vets/index"))
+                .andExpect(model().attribute("vets",hasSize(2)));
+    }
+    @Test
+    void listOfVetsIndexHTML() throws Exception {
+        when(vetService.findAll()).thenReturn(vet);
+        mockMvc.perform(get("/vets.html/index.html"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("vets/index"))
+                .andExpect(model().attribute("vets",hasSize(2)));
+    }
+}
