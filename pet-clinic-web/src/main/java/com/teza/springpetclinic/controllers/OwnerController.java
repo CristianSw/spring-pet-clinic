9f35c710b77e7a8c78e98f43e1ab48f57253223e
Cristian <cristian.sw@protonmail.com>
2020-04-17 09:53:47 +0300

    Added Owners MVC Controller tests

--- pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/OwnerController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/owners.html"})
+@RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
     private final OwnerService ownerService;
