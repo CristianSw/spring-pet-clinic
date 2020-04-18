4c0d7621285f260febc5c3c09e60d385420d1c69
Cristian <cristian.sw@protonmail.com>
2020-04-18 17:21:40 +0300

    Fixing empty find search for owners

--- pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/teza/springpetclinic/controllers/OwnerController.java
@@ -41,7 +41,7 @@ public class OwnerController {
             owner.setLastName("");
         }
 
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
         if (results.isEmpty()){
             result.rejectValue("LastName", "Not Found", "Not Found");
             return "owners/findOwners";
