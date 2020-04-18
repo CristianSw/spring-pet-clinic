6272d8c9c2c152c6bab95e24aaa62937424cb1cb
Cristian <cristian.sw@protonmail.com>
2020-04-18 17:13:30 +0300

    Added Find owners page and implementation for JPA, also provided integration tests

--- pet-clinic-data/src/main/java/com/teza/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,10 @@ package com.teza.springpetclinic.repositories;
 import com.teza.springpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-    public Owner findByLastName(String lastName);
+    Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(final String lastName);
 }
--- pet-clinic-data/src/main/java/com/teza/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/teza/springpetclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package com.teza.springpetclinic.services;
 
 import com.teza.springpetclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(final String lastName);
+
+    List<Owner> findAllByLastNameLike(final String lastName);
 }
