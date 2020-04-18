491e142b59867ccfb4c655e3424156363c4933c3
Cristian <cristian.sw@protonmail.com>
2020-04-18 20:46:51 +0300

    Fixing not working add pet by adding petType formatter

---
+++ pet-clinic-web/src/main/java/com/teza/springpetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,37 @@
+package com.teza.springpetclinic.formatters;
+
+import com.teza.springpetclinic.model.PetType;
+import com.teza.springpetclinic.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Set;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Set<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
